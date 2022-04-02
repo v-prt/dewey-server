0a380149b6d839231495f17c3d837e7e33b9a811
Victoria Peart <73324816+v-prt@users.noreply.github.com>
2022-04-02 15:18:20 -0700

    parse incoming requests

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@
 import express, { Express } from 'express'
 
 // we use the mongoose package to connect to MongoDB by appending to the URL the credentials held on the nodemon.json file
-import mongoose, { ConnectOptions } from 'mongoose'
+import mongoose from 'mongoose'
 import cors from 'cors'
 import todoRoutes from './routes'
 
@@ -10,19 +10,13 @@ const app: Express = express()
 
 const PORT: string | number = process.env.PORT || 4000
 
+// we need to parse our incoming requests
+app.use(express.json())
+app.use(express.urlencoded())
 app.use(cors())
 app.use(todoRoutes)
 
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster.gfccu.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-
-// current version of mongoose doesn't support options and sets them by default so we need to remove them
-// const options = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// } as ConnectOptions
-// mongoose.set('useFindAndModify', false)
 
 mongoose
   .connect(uri)
--- src/controllers/todos/index.ts
+++ src/controllers/todos/index.ts
@@ -15,7 +15,6 @@ const getTodos = async (req: Request, res: Response): Promise<void> => {
 const addTodo = async (req: Request, res: Response): Promise<void> => {
   try {
     const body = req.body as Pick<ITodo, 'name' | 'description' | 'status'>
-
     const todo: ITodo = new Todo({
       name: body.name,
       description: body.description,
